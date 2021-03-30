fb0f83357a9fa01d6fc5b43f85e232800e43b45b
shiitsu412 <shiitsu@hawaii.edu>
2021-03-29 21:20:29 -1000

    Assignment1

--- Shiitsu_Airi_Assignment1/MilkteaShop/products_data.js
+++ Shiitsu_Airi_Assignment1/MilkteaShop/products_data.js
@@ -1,30 +1,40 @@
 var products = [
 
-    { "item" : "Milk Tea",
-    "price" : 4.99,
-    "image" : 'Images/Milktea.png',
-    "info" : "Our Earl Gray Milk tea, with its accents of Citrus scent, is combined with Organic creamy armond milk. You can enjoy rich England afternoon tea."},
+    {
+        "item": "Milk Tea",
+        "price": 4.99,
+        "image": 'Images/Milktea.png',
+        "info": "Our Earl Gray Milk tea, with its accents of Citrus scent, is combined with Organic creamy armond milk."
+    },
 
 
-    { "item" : "Green Tea",
-    "price" : 5.99,
-    "image" : 'Images/Greentea.png',
-    "info" : "Smooth and creamy matcha sweetened just right and served with armond milk. This flavor will let you enjoy innovative Japanese green tea."},
+    {
+        "item": "Green Tea",
+        "price": 5.99,
+        "image": 'Images/Greentea.png',
+        "info": "Smooth and creamy matcha sweetened just right and served with armond milk."
+    },
 
-    { "item" : "Chocolate",
-    "price" : 5.99,
-    "image" : 'Images/Chocolate.png',
-    "info" : "Coconut milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavored drizzle. You can enjoy an acquired taste with our Chocolate."},
-
-
-    { "item" : "Strawberry Milk Tea",
-    "price" : 5.99,
-    "image" : 'Images/Strawberrymilktea.png',
-    "info" : "Our Strawberry Milk Tea, with its accents of fresh straberry, is combined with creamy coconutmilk. You can feel summer with our  fruity and refreshing sip of Strawberry Milk Tea."},
+    {
+        "item": "Chocolate",
+        "price": 5.99,
+        "image": 'Images/Chocolate.png',
+        "info": "Coconut milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavored drizzle."
+    },
 
 
-    { "item" : "Lemonade",
-    "price" : 4.99,
-    "image" : 'Images/Lemonade.png',
-    "info" : "Let's taste your lemonade you used to love. You can enjoy your familiar taste with our Lemonade."},
+    {
+        "item": "Strawberry",
+        "price": 5.99,
+        "image": 'Images/Strawberrymilktea.png',
+        "info": "Our Strawberry Milk Tea, with its accents of fresh straberry, is combined with creamy coconutmilk."
+    },
+
+
+    {
+        "item": "Lemonade",
+        "price": 4.99,
+        "image": 'Images/Lemonade.png',
+        "info": "Let's taste your lemonade you used to love. "
+    },
 ];
