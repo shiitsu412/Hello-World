02b2abd9d01ff45d0780d8d1bef9cb91d8dd4e96
shiitsu412 <shiitsu@hawaii.edu>
2021-03-30 10:04:39 -1000

    Assignment1

--- Shiitsu_Airi_Assignment1/MilkteaShop/products_data.js
+++ Shiitsu_Airi_Assignment1/MilkteaShop/products_data.js
@@ -2,7 +2,7 @@ var products = [
 
     {
         "item": "Milk Tea",
-        "price": 4.99,
+        "price": 5.00,
         "image": 'Images/Milktea.png',
         "info": "Our Earl Gray Milk tea, with its accents of Citrus scent, is combined with Organic creamy armond milk."
     },
@@ -10,14 +10,14 @@ var products = [
 
     {
         "item": "Green Tea",
-        "price": 5.99,
+        "price": 6.00,
         "image": 'Images/Greentea.png',
         "info": "Smooth and creamy matcha sweetened just right and served with armond milk."
     },
 
     {
         "item": "Chocolate",
-        "price": 5.99,
+        "price": 6.00,
         "image": 'Images/Chocolate.png',
         "info": "Coconut milk and mocha sauce topped with sweetened whipped cream and a chocolate-flavored drizzle."
     },
@@ -25,7 +25,7 @@ var products = [
 
     {
         "item": "Strawberry",
-        "price": 5.99,
+        "price": 6.00,
         "image": 'Images/Strawberrymilktea.png',
         "info": "Our Strawberry Milk Tea, with its accents of fresh straberry, is combined with creamy coconutmilk."
     },
@@ -33,7 +33,7 @@ var products = [
 
     {
         "item": "Lemonade",
-        "price": 4.99,
+        "price": 5,
         "image": 'Images/Lemonade.png',
         "info": "Let's taste your lemonade you used to love. "
     },
