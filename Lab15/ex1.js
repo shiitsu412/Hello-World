364a7ac05cc37585f48f4da220c7ed260226eb85
shiitsu412 <shiitsu@hawaii.edu>
2021-04-20 14:38:16 -1000

    15&2

---
+++ Lab15/ex1.js
@@ -0,0 +1,21 @@
+{
+    "dport": {
+        "name": "Dan Port",
+        "password": "portspassword",
+        "email": "dport@hawaii.edu"
+    },
+    "kazman": {
+        "name": "Rick Kazman",
+        "password": "kazmanpassword",
+        "email": "kazman@hawaii.edu"
+    },
+    "newuser": {
+        "password": "newpass",
+        "email": "newuser@user.com"
+    },
+    "Airi": {
+        "password": "123",
+        "email": "shiitsu@hawaii.edu",
+        "name": "Airi Shiitsu"
+    }
+}
